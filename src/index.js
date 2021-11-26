440d29503d16fc7c6e46e55b146fa87a72437272
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-26 21:11:02 +0200

    ///

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const input = document.querySelector('#search-box');
+const countryInfoCard = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry () {
+    countryInfoCard.innerHTML = '';
+    countryList.innerHTML = '';
+    const name = input.value.trim();
+    console.log(name);
+    if (name) {
+        fetchCountries(name).then(    
+
+    )
+    .catch(
+        showError()
+    )
+}
+
+function showCountryCard() {
+  
+}
+
+function showListOfCountries() {
+    
+}
+
+function showError() {
+
+}
